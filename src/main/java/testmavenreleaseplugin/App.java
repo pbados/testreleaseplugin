30ede342fa6687dd119dfeec8c91267eab54f966
Patryk Bądos <patryk.bados@billennium.pl>
2018-07-31 08:44:23 +0100

    initial commit

---
+++ src/main/java/testmavenreleaseplugin/App.java
@@ -0,0 +1,13 @@
+package testmavenreleaseplugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! version 1.0.0" );
+    }
+}
