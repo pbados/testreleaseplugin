57c69a5c51d2d0659bb41d0ac701f8210d504fc6
Patryk Bądos <patryk.bados@billennium.pl>
2018-07-31 08:47:24 +0100

    first develop commit

--- src/main/java/testmavenreleaseplugin/App.java
+++ src/main/java/testmavenreleaseplugin/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! version 1.0.0" );
+        System.out.println( "Hello World! version 1.0.0 on develop" );
+        System.out.println("Test test test");
     }
 }
