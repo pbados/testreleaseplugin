c026e9d899c76dbcfd781707e1f8b06af0fa89d5
Patryk Bądos <patryk.bados@billennium.pl>
2018-07-31 10:11:05 +0100

    .

--- src/main/java/testmavenreleaseplugin/App.java
+++ src/main/java/testmavenreleaseplugin/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! version 1.0.1 on develop" );
+        System.out.println( "Hello World! version 1.0.3 on develop" );
         System.out.println("Test test test");
     }
 }
